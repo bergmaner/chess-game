749283a54ff804c2f3845c1175affa8c61c05586
bergk <kab@assemble.world>
2025-01-07 20:34:49 +0100

    basic drag and drop

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
+let legalSquares=[];
+const boardSquares = document.getElementsByClassName('square');
+const pieces = document.getElementsByClassName('piece');
+const piecesImages = document.getElementsByClassName('pieceImg');
+
+setupBoardSquares();
+setupPieces();
+function setupBoardSquares()  {
+    for (let i=0; i<boardSquares.length; i++){
+        boardSquares[i].addEventListener('dragover',allowDrop);
+        boardSquares[i].addEventListener('drop',drop);
+        let row= 8 - Math.floor(i/8);
+        let column = String.fromCharCode(97 + (i%8));
+        let square = boardSquares[i];
+        square.id = column + row;
+
+    }
+}
+
+function setupPieces()  {
+
+    for(let i=0; i<pieces.length; i++){
+        pieces[i].addEventListener('dragstart', drag);
+        pieces[i].setAttribute('draggable', true);
+        pieces[i].id = pieces[i].className.split(" ")[1] + pieces[i].parentElement.id;
+    }
+
+    for(let i=0; i<piecesImages.length; i++){
+        piecesImages[i].setAttribute('draggable', false);
+    }
+
+}
+
+function allowDrop(ev){
+    ev.preventDefault()
+}
+
+function drag(ev){
+ const piece = ev.target;
+ ev.dataTransfer.setData("text", piece.id);
+}
+
+function drop(ev){
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    const piece = document.getElementById(data);
+    const destinationSquare = ev.currentTarget;
+    let destinationSquareId = destinationSquare.id;
+    destinationSquare.appendChild(piece)
+}
+
+
 
 
 
